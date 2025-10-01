16b09f22e735454fbb518c6791b905a93ac12423
peter mbiyu <petersmasha@gmail.com>
2025-10-01 06:07:50 +0300

    day 1 commit and push

--- readMe.md
+++ readMe.md
@@ -1,4 +1,4 @@
-#curriculum overview
+# curriculum overview
 
 1. creating a server
 2. sending status codes (200, 404)
@@ -6,3 +6,11 @@
 4. handling request/response
 5. filtering data
 6. extracting path/query params
+
+## The request object
+
+- req.body = data from the request body
+- req.params - search params eg api/continent/country
+- req.method - HTTP method eg POST, GET DELETE
+- req.ip - client's ip
+- req.query - query params eg api?name=peter&status=married
--- server.js
+++ server.js
@@ -1,17 +1,66 @@
 import express from "express";
 import cors from "cors";
+import { startups } from "./data.js";
 
 // server creation
 const PORT = 8000;
 const app = express();
-app.listen(PORT, "localhost", () => console.log(`Connected at port ${PORT}`));
+app
+  .listen(PORT, "localhost", () => console.log(`Connected at port ${PORT}`))
+  .on("error", () => console.log("failed to connect"));
 
 //middleware
 app.use(cors());
 
 // routing
 app.get("/api", (req, res) => {
-  res.status(200).json({ message: "Hello from the other side" });
+  let filteredData = startups;
+  const { industry, country, continent, is_seeking_funding, has_mvp } =
+    req.query;
+  if (industry) {
+    filteredData = filteredData.filter(
+      (data) => data.industry.toLowerCase() === industry.toLowerCase()
+    );
+  }
+  if (country) {
+    filteredData = filteredData.filter(
+      (data) => data.country.toLowerCase() === country.toLowerCase()
+    );
+  }
+  if (continent) {
+    filteredData = filteredData.filter(
+      (data) => data.continent.toLowerCase() === continent.toLowerCase()
+    );
+  }
+  if (is_seeking_funding) {
+    filteredData = filteredData.filter(
+      (data) =>
+        data.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase())
+    );
+  }
+  if (has_mvp) {
+    filteredData = filteredData.filter(
+      (data) => data.has_mvp === JSON.parse(has_mvp.toLowerCase())
+    );
+  }
+
+  res.status(200).json(filteredData);
+});
+app.get("/api/:field/:term", (req, res) => {
+  let filteredData = startups;
+  const { field, term } = req.params;
+  const allowedField = ["country", "continent", "industry"];
+  if (!allowedField.includes(field)) {
+    filteredData = {
+      message:
+        "Search field not allowed. Please use only 'country','continent', 'industry'",
+    };
+    return res.status(400).json(filteredData);
+  }
+  filteredData = filteredData.filter(
+    (startup) => startup[field].toLowerCase() === term.toLowerCase()
+  );
+  res.status(200).json(filteredData);
 });
 app.use((req, res) => {
   res.status(404).json({ message: "Route not found.. :(" });
